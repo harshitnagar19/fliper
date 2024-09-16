6c9b9739a147dde38cb88466026ffcc97bb5c593
harshit nagar <nharshit7024@gmail.com>
2024-09-16 20:34:57 +0530

    changes done

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // ******************************add database url below***********************************************
 
 MONGODB_URI = 'mongodb+srv://nharshit216:Reena%40123@cluster0.ohdfl.mongodb.net/'                   
-mongoose.connect(MONGODB_URI,{connectTimeoutMS:30000}).then(res => { 
+mongoose.connect(MONGODB_URI).then(res => { 
     console.log("connected to database server successfully")
 }).catch(err => {
     console.log("error while connecting  to dataBase",err)
