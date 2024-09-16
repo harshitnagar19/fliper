8f61208a0cafb59bf02f545f6f5c2d7cc9e9badf
harshit nagar <nharshit7024@gmail.com>
2024-09-16 20:15:04 +0530

    changes done

--- server/services/UserService.js
+++ server/services/UserService.js
@@ -3,7 +3,12 @@ const User = require("../models/User")
 const UserService = {}
 
 UserService.findUserWithEmail = async (email) => {
-    return (await User.findOne({ email }))
+        try{
+           return  await User.findOne({ email })
+        }catch(err){
+            return null
+        }
+
 }
 
 module.exports = UserService
