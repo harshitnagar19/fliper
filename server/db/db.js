b02645d84e878bc2b5dac4f6cc1e275c84c30523
harshit nagar <nharshit7024@gmail.com>
2024-09-16 20:46:18 +0530

    changes done

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // ******************************add database url below***********************************************
 
-MONGODB_URI = 'mongodb+srv://nharshit216:Reena%40123@cluster0.ohdfl.mongodb.net/'                   
+MONGODB_URI = 'mongodb+srv://nharshit216:Reena@123@cluster0.ohdfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'                   
 mongoose.connect(MONGODB_URI).then(res => { 
     console.log("connected to database server successfully")
 }).catch(err => {
