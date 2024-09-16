aac0688959009ae222c3ef1803eac13044596891
harshit nagar <nharshit7024@gmail.com>
2024-09-16 20:53:37 +0530

    done

--- server/db/db.js
+++ server/db/db.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 // ******************************add database url below***********************************************
 
-MONGODB_URI = 'mongodb+srv://nharshit216:Reena@123@cluster0.ohdfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'                   
-mongoose.connect(MONGODB_URI).then(res => { 
-    console.log("connected to database server successfully")
-}).catch(err => {
-    console.log("error while connecting  to dataBase",err)
-}) 
+const MONGODB_URI = 'mongodb+srv://nharshit216:Reena%40123@cluster0.ohdfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connected to database server successfully");
+  })
+  .catch(err => {
+    console.log("error while connecting to database", err);
+  });
    
